eb1dd1dfb5871f332ce5d497b6acf2071ea99083
esthrus <ma_xxd@163.com>
2020-12-31 23:41:35 +0800

    Update some files

--- SparkTask2/Proportion.scala
+++ SparkTask2/Proportion.scala
@@ -21,26 +21,29 @@ object Proportion {
     val header1 = infordd.first
     val info = infordd.filter(line => line != header1)
     //(user_id, gender)
-    val user_gender = info.map(line => line.split(","))
+    val user_info = info.map(line => line.split(","))
       .filter(line => line.length == 3)
-      .filter(line => line(2) != "2")
-      .map(line => (line(0), line(2)))
+      .filter(line => line(1) != "0") //筛除年龄未知
+      .filter(line => line(2) != "2") //筛除性别未知
+      .map(line => (line(0), (line(1), line(2))))
 
     val logrdd = sc.textFile("data/user_log_format1.csv")
     val header2 = logrdd.first
     val log = logrdd.filter(line => line != header2)
-    val user_action = log.map(line => line.split(","))
+    val user_log = log.map(line => line.split(","))
       .filter(line => line.length == 7)
-      .filter(line => line(6) == "2")
-      .map(line => (line(0), line(6)))
+      .filter(line => line(5) == "1111") //筛选双十一
+      .filter(line => line(6) == "2") //筛选购买
+      .map(line => (line(0), line(6))) //(user_id,action_type)
+      .distinct //去重
 
     //统计买了商品的买家
-    user_gender.rightOuterJoin(user_action)
-    val maleCount = user_gender.filter(line => line._2 == "1").count()
+    val count = user_info.join(user_log) //(user_id,<(age,gender),action_type>)
+    val maleCount = count.filter(line => line._2._1._2 == "1").count()
     println("Male:" + maleCount)
-    val femaleCount = user_gender.filter(line => line._2 == "0").count()
+    val femaleCount = count.filter(line => line._2._1._2 == "0").count()
     println("Female:" + femaleCount)
-    val gendercount = user_gender.count()
+    val gendercount = count.count()
     println("Total users:" + gendercount)
 
     //统计男女比例
@@ -48,21 +51,15 @@ object Proportion {
     println("Female proportion:" + femaleCount.toDouble / gendercount.toDouble)
     println("-" * 40)
 
-    //(user_id,age_range)
-    val user_age = info.map(line => line.split(","))
-      .filter(line => line.length == 3)
-      .filter(line => line(1) != "0")
-      .filter(line => line(1) != "")
-      .map(line => (line(0), line(1)))
-    user_age.rightOuterJoin(user_action)
-    val agecount = user_age.count()
-    val range1 = user_age.filter(line => line._2 == "1").count()
-    val range2 = user_age.filter(line => line._2 == "2").count()
-    val range3 = user_age.filter(line => line._2 == "3").count()
-    val range4 = user_age.filter(line => line._2 == "4").count()
-    val range5 = user_age.filter(line => line._2 == "5").count()
-    val range6 = user_age.filter(line => line._2 == "6").count()
-    val range7 = user_age.filter(line => line._2 == "7" || line._2 == "8").count()
+    //统计年龄段比例
+    val range1 = count.filter(line => line._2._1._1 == "1").count()
+    val range2 = count.filter(line => line._2._1._1 == "2").count()
+    val range3 = count.filter(line => line._2._1._1 == "3").count()
+    val range4 = count.filter(line => line._2._1._1 == "4").count()
+    val range5 = count.filter(line => line._2._1._1 == "5").count()
+    val range6 = count.filter(line => line._2._1._1 == "6").count()
+    val range7 = count.filter(line => line._2._1._1 == "7" || line._2._1._1 == "8").count()
+    val agecount = count.count()
     println("<18:" + range1 + ", proportion=" + range1.toDouble / agecount.toDouble)
     println("[18,24]:" + range2 + ", proportion=" + range2.toDouble / agecount.toDouble)
     println("[25,29]:" + range3 + ", proportion=" + range3.toDouble / agecount.toDouble)
@@ -71,7 +68,6 @@ object Proportion {
     println("[40,49]:" + range6 + ", proportion=" + range6.toDouble / agecount.toDouble)
     println(">=50:" + range7 + ", proportion=" + range7.toDouble / agecount.toDouble)
     println("Total age ranges:" + agecount)
-
 
     sc.stop()
   }
